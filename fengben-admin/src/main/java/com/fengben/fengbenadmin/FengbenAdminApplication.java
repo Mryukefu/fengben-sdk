27bc7ee0d6195bcdccc3d7f272b6b5667b7a3997
yukefu <15812784862@163.com>
2022-05-20 18:15:37 +0800

    eee

--- fengben-admin/src/main/java/com/fengben/fengbenadmin/FengbenAdminApplication.java
+++ fengben-admin/src/main/java/com/fengben/fengbenadmin/FengbenAdminApplication.java
@@ -2,8 +2,10 @@ package com.fengben.fengbenadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FengbenAdminApplication {
 
     public static void main(String[] args) {
---
+++ fengben-admin/src/main/java/com/fengben/fengbenadmin/controller/TestController.java
@@ -0,0 +1,9 @@
+package com.fengben.fengbenadmin.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+}
